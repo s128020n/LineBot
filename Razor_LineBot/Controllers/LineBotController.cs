b36d1a28b5854f103eb6a912a827b19e08804149
s128020@gmail.com <s128020@gmail.com>
2020-02-01 21:48:35 +0800

    Add project files.

---
+++ Razor_LineBot/Controllers/LineBotController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace LineBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineBotController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public LineBotController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public IActionResult POST()
+        {
+            //get configuration from appsettings.json
+            var token = _config.GetSection("LINE-Bot-Setting:channelAccessToken");
+            var AdminUserId = _config.GetSection("LINE-Bot-Setting:adminUserID");
+            var body = ""; //for JSON Body
+            //create vot instance
+            var bot = new isRock.LineBot.Bot(token.Value);
+            isRock.LineBot.MessageBase responseMsg = null;
+            //message collection for response multi-message 
+            List<isRock.LineBot.MessageBase> responseMsgs =
+                new List<isRock.LineBot.MessageBase>();
+
+            try
+            {
+                //get JSON Body
+                using (StreamReader reader = new StreamReader(Request.Body, System.Text.Encoding.UTF8))
+                {
+                    body = reader.ReadToEndAsync().Result;
+                }
+                //parsing JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(body);
+                //Get LINE Event
+                var LineEvent = ReceivedMessage.events.FirstOrDefault();
+                //prepare reply message
+                if (LineEvent.type.ToLower() == "message")
+                {
+                    switch (LineEvent.message.type.ToLower())
+                    {
+                        case "text":
+                            //add text response
+                            responseMsg =
+                                new isRock.LineBot.TextMessage($"you said : {LineEvent.message.text}");
+                            responseMsgs.Add(responseMsg);
+                            //add ButtonsTemplate if user say "/Show ButtonsTemplate"
+                            if (LineEvent.message.text.ToLower().Contains("/show buttonstemplate"))
+                            {
+                                //define actions
+                                var act1 = new isRock.LineBot.MessageAction()
+                                { text = "test action1", label = "test action1" };
+                                var act2 = new isRock.LineBot.MessageAction()
+                                { text = "test action2", label = "test action2" };
+
+                                var tmp = new isRock.LineBot.ButtonsTemplate()
+                                {
+                                    text = "Button Template text",
+                                    title = "Button Template title",
+                                    thumbnailImageUrl = new Uri("https://i.imgur.com/wVpGCoP.png"),
+                                };
+
+                                tmp.actions.Add(act1);
+                                tmp.actions.Add(act2);
+                                //add TemplateMessage into responseMsgs
+                                responseMsgs.Add(new isRock.LineBot.TemplateMessage(tmp));
+                            }
+                            break;
+                        case "sticker":
+                            responseMsg =
+                            new isRock.LineBot.StickerMessage(1, 2);
+                            responseMsgs.Add(responseMsg);
+                            break;
+                        default:
+                            responseMsg = new isRock.LineBot.TextMessage($"None handled message type : { LineEvent.message.type}");
+                            responseMsgs.Add(responseMsg);
+                            break;
+                    }
+                }
+                else
+                {
+                    responseMsg = new isRock.LineBot.TextMessage($"None handled event type : { LineEvent.type}");
+                    responseMsgs.Add(responseMsg);
+                }
+
+                //回覆訊息
+                bot.ReplyMessage(LineEvent.replyToken, responseMsgs);
+                //response OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //如果有錯誤，push給admin
+                bot.PushMessage(AdminUserId.Value, "Exception : \n" + ex.Message);
+                //response OK
+                return Ok();
+            }
+        }
+    }
+}
---
+++ Razor_LineBot/Pages/Index.cshtml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Razor_LineBot.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string sNote = "";
+        private readonly IConfiguration _config;
+        public IndexModel(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        [BindProperty]
+        public string txbToken { get; set; }
+        [BindProperty]
+        public string txbUserId { get; set; }
+        [BindProperty]
+        public string txbMessage { get; set; }
+        [BindProperty]
+        public int txbStickerPkgId { get; set; }
+        [BindProperty]
+        public int txbStickerStkId { get; set; }
+        public void OnGet()
+        {
+            //get configuration from appsettings.json
+            var token = _config.GetSection("LINE-Bot-Setting:channelAccessToken");
+            var AdminUserId = _config.GetSection("LINE-Bot-Setting:adminUserID");
+            txbToken = token.Value;
+            txbUserId = AdminUserId.Value;
+            txbMessage = "";
+            return;
+        }
+
+        public void OnPostSendMessage()
+        {
+            if (string.IsNullOrEmpty(txbToken) || string.IsNullOrEmpty(txbUserId) || string.IsNullOrEmpty(txbMessage))
+            {
+                sNote = "Cannot sent the message due to missing data.";
+            }
+            else
+            {
+                var bot = new isRock.LineBot.Bot(txbToken);
+
+                try
+                {
+                    var ret = bot.PushMessage(txbUserId, txbMessage);
+                    sNote = "Success !";
+                }
+                catch (Exception ex)
+                {
+                    this.sNote = " The token / The user ID was not  found.";
+                }
+            }
+        }
+        public void OnPostSendSticker()
+        {
+            if (string.IsNullOrEmpty(txbToken) || string.IsNullOrEmpty(txbUserId))
+            {
+                sNote = "Cannot sent the message due to missing data.";
+            }
+            else
+            {
+                var bot = new isRock.LineBot.Bot(txbToken);
+
+                try
+                {
+                    var ret = bot.PushMessage(txbUserId, new isRock.LineBot.StickerMessage(txbStickerPkgId, txbStickerStkId));
+                    sNote = "Success !";
+                }
+                catch (Exception ex)
+                {
+                    this.sNote = " The token / The user ID or The sticker ID was not  found .";
+                }
+            }
+        }
+    }
+}
